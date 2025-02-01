2c8772a36f81da1af9377ab4498807d0aa9631a6
Safvan Husain <developer.safvan@gmail.com>
2025-02-01 14:22:56 +0530

    updated getNotifications to send total items instead of pages

--- src/controllers/notification/notificationController.ts
+++ src/controllers/notification/notificationController.ts
@@ -18,7 +18,7 @@ export const getNotificationsForBusiness = asyncHandler(
             res.status(200).json({
                 notifications,
                 currentPage: page,
-                totalPages: Math.ceil(total / limit),
+                totalItems:total,
             });
         } catch (error) {
             console.log("error ar getNotification", error);
