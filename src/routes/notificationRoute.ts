42103a494f4e544ca9758e3de4514f30cad99abc
Safvan Husain <developer.safvan@gmail.com>
2025-02-05 16:18:47 +0530

    test notification

--- src/routes/notificationRoute.ts
+++ src/routes/notificationRoute.ts
@@ -5,17 +5,30 @@ import {
     getNotificationsForBusiness, getNotificationsForUser
 } from "../controllers/notification/notificationController";
 import { admin } from "../middleware/auth";
+import Store from "../models/storeModel";
 
 const router = Router();
 
 router.route('/').delete(admin, deleteNotification);
 
+//TODO: add middlewares.
 router.route('/user')
     .post(admin, createNotificationForUsers)
     .get(getNotificationsForUser);
 router.route('/business')
-    .post(admin, createNotificationForBusiness)
+    .post(createNotificationForBusiness)
     .get(getNotificationsForBusiness);
+
+router.get('/tokens', async (req: any, res: any) => {
+    try {
+        const stores = Store.find({
+            fcmToken: { $exist: true }
+        });
+        res.status(200).json(stores);
+    } catch (error) {
+        res.status(500).json({ message: error});
+    }
+})    
 
 
 export { router as notificationRouter };
