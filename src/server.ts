c17cc927848e0322ddb7b93bf0314be88909acd1
Safvan Husain <developer.safvan@gmail.com>
2025-02-19 17:12:52 +0530

    fix

--- src/server.ts
+++ src/server.ts
@@ -60,57 +60,23 @@ app.use("/api/healthcheck", (req, res) => {
   res.status(200).send("Server is healthy");
 });
 
-// async function correctCoordinates() {
-//   // Fetch all documents with location data
-//   var result = await Store.find({}, { location: true, location_v2: true });
-
-//   // Iterate through each document
-//   for (let doc of result) {
-//     console.log(doc);
-
-//     let coordinates = doc.location_v2.coordinates;
-//     console.log(doc.location_v2.coordinates, " ", doc.location_v2.coordinates);
-
-//     await Store.updateOne(
-//         { _id: doc._id },
-//         { $set: { "location.coordinates": coordinates } }
-//       );
-//   }
-
-//   console.log("Coordinates have been corrected.");
-// }
 async function correctCoordinates() {
   // Fetch all documents with location data
-  var result = await Store.find({}, { location: true });
+  var result = await Store.find({}, { location: true, location_v2: true });
 
   // Iterate through each document
   for (let doc of result) {
-    let coordinates = doc.location.coordinates;
+    let coordinates = doc.location_v2.coordinates;
 
-    // Check if the first item is greater than 70 (likely longitude)
-    if (coordinates[0] < 70) {
-      await Store.updateOne(
+    await Store.updateOne(
         { _id: doc._id },
-        { $set: { "location_v2.coordinates": coordinates } }
+        { $set: { "location.coordinates": coordinates } }
       );
-      // If the first item is longitude, no need to switch
-      continue;
-    } else {
-      // If the first item is not longitude, switch the coordinates
-      let temp = coordinates[0];
-      coordinates[0] = coordinates[1];
-      coordinates[1] = temp;
-
-      // Update the document in the database
-      await Store.updateOne(
-        { _id: doc._id },
-        { $set: { "location_v2.coordinates": coordinates } }
-      );
-    }
   }
 
   console.log("Coordinates have been corrected.");
 }
+
 
 async function addLocationFieldToAllProducts() {
   try {
