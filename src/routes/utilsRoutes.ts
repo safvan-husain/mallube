28335bd52fabb664bad0b3bf17b15ed4bd65a9e3
Safvan Husain <developer.safvan@gmail.com>
2025-03-06 12:36:45 +0530

    upload image fix

--- src/routes/utilsRoutes.ts
+++ src/routes/utilsRoutes.ts
@@ -12,7 +12,7 @@ router.route("/upload").post(function (request, response) {
   if (!file) return response.sendStatus(400);
 
   // Destructure the content of the file object
-  if (Array.isArray(file)) return response.status(400).json({ error: "Multiple files are not allowed" })
+  // if (Array.isArray(file)) return response.status(400).json({ error: "Multiple files are not allowed" })
   const { name, mimetype, size, data } = file;
   // If `data` is a string, specify the correct encoding (e.g., 'utf8')
   const fileContent = Buffer.isBuffer(data) ? data : Buffer.from(data, "utf8");
