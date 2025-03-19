def6551fb3ea4fa5aa3d1ea505ce7d1264d87560
Safvan Husain <developer.safvan@gmail.com>
2025-03-19 14:54:49 +0530

    added DisplayCategory.ts added to show on home screen

---
+++ src/models/DisplayCategory.ts
@@ -0,0 +1,44 @@
+import {Schema, model, Document, ObjectId} from "mongoose";
+
+export interface IDisplayCategory extends Document {
+    name: string;
+    // -1 means it is not enabled for business [home screen of the app]
+    businessIndex: number;
+    // -1 means it is not enabled for freelancer [freelance section]
+    freelancerIndex: number;
+    icon: string;
+    categories: ObjectId[];
+}
+
+const displayCategorySchema = new Schema<IDisplayCategory>(
+    {
+        name: {
+            type: String,
+            required: true,
+        },
+        icon: {
+            type: String,
+        },
+        businessIndex: {
+            type: Number,
+            default: -1,
+        },
+        freelancerIndex: {
+            type: Number,
+            default: -1,
+        },
+        categories: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: "categories",
+            },
+        ],
+    },
+    {
+        timestamps: true,
+    }
+);
+
+const DisplayCategory = model<IDisplayCategory>("categories", displayCategorySchema);
+
+export default DisplayCategory;
