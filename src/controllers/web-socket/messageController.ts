44bdf63051265704c86468ab6280e7c102afa783
Safvan Husain <developer.safvan@gmail.com>
2025-03-26 18:50:30 +0530

    delete conversation api added.

--- src/controllers/web-socket/messageController.ts
+++ src/controllers/web-socket/messageController.ts
@@ -1,38 +1,39 @@
-import { Request, Response, Router } from "express";
+import {Request, Response, Router} from "express";
 import asyncHandler from "express-async-handler";
-import { z } from "zod";
-import { ICustomRequest } from "../../types/requestion";
-import { Message } from "../../models/messageModel";
-import { Types } from "mongoose";
+import {z} from "zod";
+import {ICustomRequest} from "../../types/requestion";
+import {Message} from "../../models/messageModel";
+import {Types} from "mongoose";
 import * as s from './webSocketController';
-import { onCatchError } from "../service/serviceContoller";
-import { Chat } from "../../models/chatsModel";
+import {onCatchError} from "../service/serviceContoller";
+import {Chat} from "../../models/chatsModel";
 import User from "../../models/userModel";
+import {ObjectIdSchema} from "../../types/validation";
 
 export const getConversation = asyncHandler(
     async (req: ICustomRequest<any>, res: Response) => {
         const userId = req.user?._id;
-        
+
         const otherUserId = req.params.otherUserId;
-        console.log(userId, " - " ,otherUserId);
+        console.log(userId, " - ", otherUserId);
         if (!Types.ObjectId.isValid(otherUserId)) {
-            res.status(400).json({ message: "Invalid user id" })
+            res.status(400).json({message: "Invalid user id"})
         }
         try {
             const messages = await Message.find({
                 $or: [
-                    { senderId: userId, receiverId: otherUserId },
-                    { senderId: otherUserId, receiverId: userId }
+                    {senderId: userId, receiverId: otherUserId},
+                    {senderId: otherUserId, receiverId: userId}
                 ]
-            }).sort({ createdAt: 1 }).lean();
+            }).sort({createdAt: 1}).lean();
             await Message.updateMany({
                 $or: [
-                    { senderId: userId, receiverId: otherUserId },
-                    { senderId: otherUserId, receiverId: userId }
+                    {senderId: userId, receiverId: otherUserId},
+                    {senderId: otherUserId, receiverId: userId}
                 ],
                 isRead: false
             }, {
-                $set: { isRead: true }
+                $set: {isRead: true}
             }).lean();
             res.status(200).json(messages.map(e => ({...e, timestamp: e.timestamp.getTime()})));
         } catch (error) {
@@ -45,13 +46,13 @@ export const getChats = asyncHandler(
     async (req: ICustomRequest<any>, res: Response) => {
         const userId = req.user?._id;
         try {
-            const chats = await Chat.find({ participants: { $in: [userId] } }).populate('lastMessage').lean();
+            const chats = await Chat.find({participants: {$in: [userId]}}).populate('lastMessage').lean();
             let result = await Promise.all(
                 chats.map(async (e: any) => {
                     let otherUser: any = e.participants.find((k: any) => k.toString() !== userId?.toString());
                     if (!otherUser) return null; // Handle edge case where no other user is found
 
-                    otherUser = await User.findById(otherUser, { fullName: true }).lean();
+                    otherUser = await User.findById(otherUser, {fullName: true}).lean();
                     if (!otherUser) return null; // Handle case where user is not found in DB
 
                     return {
@@ -81,5 +82,30 @@ export const saveMessage = async (message: s.Message) => {
         });
     } catch (error) {
         console.log(error);
+    }
+}
+
+export const deleteConversation = async (req: ICustomRequest<any>, res: Response) => {
+    try {
+        let userId = req.user?._id;
+        if (!userId) {
+            res.status(400).json({message: "Invalid user id"});
+            return;
+        }
+        let otherUserIds = z.object({
+            chatIds: z.array(ObjectIdSchema)
+        }).parse(req.body);
+
+        let chats = await Chat.updateMany({
+            $and: [
+                {participants: {$in: userId}},
+                {participants: {$in: otherUserIds}}
+            ]
+        }, {
+            $addToSet: {deletedParticipants: userId}
+        })
+
+    } catch (e) {
+        onCatchError(e, res);
     }
 }
--- src/models/chatsModel.ts
+++ src/models/chatsModel.ts
@@ -1,9 +1,11 @@
 // src/models/chatsModel.ts
 
 import { Document, model, Schema, Types } from "mongoose";
+import {Message} from "./messageModel";
 
 export interface IChat extends Document {
   participants: Types.ObjectId[];
+  deletedParticipants: Types.ObjectId[];
   lastMessage: Schema.Types.ObjectId;
 }
 
@@ -15,11 +17,35 @@ const chatSchema = new Schema<IChat>({
       required: true,
     },
   ],
+  deletedParticipants: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "users",
+      required: true,
+    },
+  ],
   lastMessage: {
     type: Schema.Types.ObjectId,
     ref: "messages",
     required: true,
   },
+});
+
+chatSchema.pre("save", async function (next) {
+  if (this.deletedParticipants.length === 2) {
+    // Delete all messages related to this chat
+    await Message.deleteMany({
+      $or: [
+        {senderId: this.deletedParticipants[0], receiverId: this.deletedParticipants[1] },
+        {senderId: this.deletedParticipants[1], receiverId: this.deletedParticipants[0] },
+      ]
+    });
+
+    // Delete the chat itself
+    await this.deleteOne();
+    return; // Stop further processing
+  }
+  next();
 });
 
 export const Chat = model<IChat>("chats", chatSchema);
