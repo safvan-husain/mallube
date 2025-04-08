bd1e29cb32334255446ccbf430dfa88c1252b446
Safvan Husain <developer.safvan@gmail.com>
2025-04-08 13:25:05 +0530

    valid response for favourite shop / freelancer

--- src/controllers/user/favourites/favouritesController.ts
+++ src/controllers/user/favourites/favouritesController.ts
@@ -1,48 +1,52 @@
-import { Request, Response } from "express";
+import {Request, Response} from "express";
 import asyncHandler from "express-async-handler";
-import { UserFavRequestValidationSchema } from "./validation";
+import {UserFavRequestValidationSchema} from "./validation";
 import {ICustomRequest, TypedResponse} from "../../../types/requestion";
 import User from "../../../models/userModel";
-import { onCatchError } from "../../service/serviceContoller";
+import {internalRunTimeResponseValidation, onCatchError} from "../../service/serviceContoller";
 import Store from "../../../models/storeModel";
-import { Freelancer } from "../../../models/freelancerModel";
-import { Types } from "mongoose";
+import {Freelancer} from "../../../models/freelancerModel";
+import {Types} from "mongoose";
 import Product from "../../../models/productModel";
 import UserProduct, {UserProductResponse} from "../../../models/user_product";
 import {calculateDistance} from "../../../utils/interfaces/common";
 import {z} from "zod";
 import {locationSchema} from "../buy_and_sell/validation";
+import {StoreDetailsResponse, StoreDetailsSchema} from "../userController";
 
 
 export const toggleFavorite = asyncHandler(async (req: ICustomRequest<any>, res: Response) => {
     try {
         const userId = req.user?._id;
-        const { shopId, freelancerId, userProduct } = UserFavRequestValidationSchema.parse(req.body);
+        const {shopId, freelancerId, userProduct} = UserFavRequestValidationSchema.parse(req.body);
 
         const fieldKey = shopId ? "favouriteShops" : freelancerId ? "favouriteFreelancers" : userProduct ? "favouriteUserProducts" : null;
         const fieldValue = (shopId || freelancerId || userProduct) as Types.ObjectId;
 
         if (!fieldKey) {
-            res.status(400).json({ message: "Invalid request" })
+            res.status(400).json({message: "Invalid request"})
             return;
         }
 
-        const user = await User.findById(userId, { [fieldKey]: 1 });
-        if (!user){
-            res.status(404).json({ message: "User not found" });
+        const user = await User.findById(userId, {[fieldKey]: 1});
+        if (!user) {
+            res.status(404).json({message: "User not found"});
             return;
-        } 
+        }
 
         // Check if item is already in favorites
         const isFavourited = user[fieldKey]?.includes(fieldValue);
 
         // Toggle favorite status
         const update = isFavourited
-            ? { $pull: { [fieldKey]: fieldValue } } // Remove if already favorited
-            : { $addToSet: { [fieldKey]: fieldValue } }; // Add if not favorited
+            ? {$pull: {[fieldKey]: fieldValue}} // Remove if already favorited
+            : {$addToSet: {[fieldKey]: fieldValue}}; // Add if not favorited
 
         await User.findByIdAndUpdate(userId, update);
-        res.status(200).json({ message: isFavourited ? "Removed from favorites" : "Added to favorites", isFavourited: !isFavourited });
+        res.status(200).json({
+            message: isFavourited ? "Removed from favorites" : "Added to favorites",
+            isFavourited: !isFavourited
+        });
     } catch (error) {
         onCatchError(error, res);
     }
@@ -50,12 +54,12 @@ export const toggleFavorite = asyncHandler(async (req: ICustomRequest<any>, res:
 
 
 export const getFavoriteShops = asyncHandler(
-    async (req: ICustomRequest<any>, res: Response) => {
+    async (req: ICustomRequest<any>, res: TypedResponse<StoreDetailsResponse[]>) => {
         try {
             const userId = req.user?._id;
             const user = await User.findById(userId);
             if (!user) {
-                res.status(404).json({ message: "User not found" });
+                res.status(404).json({message: "User not found"});
                 return;
             }
 
@@ -63,8 +67,18 @@ export const getFavoriteShops = asyncHandler(
                 res.status(404).json([]);
                 return;
             }
-            const shops = await Store.find({ _id: { $in: user.favouriteShops } });
-            res.status(200).json(shops);
+            let responseList: StoreDetailsResponse[] = [];
+            const shops = await Store.find({_id: {$in: user.favouriteShops}});
+            for (const shop of shops) {
+                //TODO: correct this for type safety.
+                const validation = internalRunTimeResponseValidation<StoreDetailsResponse>(StoreDetailsSchema as any, shop as any);
+                if (validation.error) {
+                    res.status(500).json(validation.error);
+                    return;
+                }
+                responseList.push(validation.data)
+            }
+            res.status(200).json(responseList);
         } catch (error) {
             onCatchError(error, res);
         }
@@ -72,20 +86,30 @@ export const getFavoriteShops = asyncHandler(
 )
 
 export const getFavoriteFreelancers = asyncHandler(
-    async (req: ICustomRequest<any>, res: Response) => {
+    async (req: ICustomRequest<any>, res: TypedResponse<StoreDetailsResponse[]>) => {
         try {
             const userId = req.user?._id;
             const user = await User.findById(userId);
             if (!user) {
-                res.status(404).json({ message: "User not found" });
+                res.status(404).json({message: "User not found"});
                 return;
             }
             if (user.favouriteFreelancers.length === 0) {
                 res.status(404).json([]);
                 return;
             }
-            const freelancers = await Store.find({ _id: { $in: user.favouriteFreelancers } });
-            res.status(200).json(freelancers);
+            let responseList: StoreDetailsResponse[] = [];
+            const freelancers = await Store.find({_id: {$in: user.favouriteFreelancers}});
+            for (const freelancer of freelancers) {
+                //TODO: correct this for type safety.
+                const validation = internalRunTimeResponseValidation<StoreDetailsResponse>(StoreDetailsSchema as any, freelancer as any);
+                if (validation.error) {
+                    res.status(500).json(validation.error);
+                    return;
+                }
+                responseList.push(validation.data)
+            }
+            res.status(200).json(responseList);
         } catch (error) {
             onCatchError(error, res);
         }
@@ -97,16 +121,18 @@ export const getFavoriteUserProducts = asyncHandler(
         try {
             const userId = req.user?._id;
             const data = locationSchema.parse(req.query);
-            const user = await User.findById(userId, { favouriteUserProducts: 1 });
+            const user = await User.findById(userId, {favouriteUserProducts: 1});
             if (!user) {
-                res.status(404).json({ message: "User not found" });
+                res.status(404).json({message: "User not found"});
                 return;
             }
             if (user.favouriteUserProducts.length === 0) {
                 res.status(404).json([]);
                 return;
             }
-            const products = await UserProduct.find({ _id: { $in: user.favouriteUserProducts } }).populate<{owner: {phone: string, _id: string}}>('owner', 'phone');
+            const products = await UserProduct.find({_id: {$in: user.favouriteUserProducts}}).populate<{
+                owner: { phone: string, _id: string }
+            }>('owner', 'phone');
             res.status(200).json(products.map(e => {
                 const distance = calculateDistance(e.location.coordinates[0], e.location.coordinates[1], data.latitude, data.longitude);
                 return e.forResponse(distance, e.owner.phone, e.owner._id);
